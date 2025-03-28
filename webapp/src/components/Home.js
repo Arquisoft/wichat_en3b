1cd8eabe9dc162f6d0e7ceebaaf681674e0e647c
Vicxami <vicxami@gmail.com>
2025-03-28 22:02:54 +0100

    Regenerated package-lock to remove unused modules. Created Home component and modified the routes

---
+++ webapp/src/components/Home.js
@@ -0,0 +1,13 @@
+import { Container, Typography } from "@mui/material";
+
+const Home = () => {
+    return (
+        <Container>
+            <Typography variant="h4" component="h1" gutterBottom>
+                Welcome to the Game App
+            </Typography>
+        </Container>
+    );
+}
+
+export default Home;
