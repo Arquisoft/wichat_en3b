0b5811948dc945db8d6ed2aaf3644be0d2a42409
Daniel Rodríguez Fernández <UO293655@uniovi.es>
2025-04-09 23:27:19 +0200

    fixed conflict with master branch

--- webapp/src/App.test.js
+++ webapp/src/App.test.js
@@ -1,8 +1,16 @@
+import React from "react";
 import { render, screen } from '@testing-library/react';
 import { MemoryRouter } from 'react-router';
 import App from './App';
 import { I18nextProvider } from "react-i18next";
 import i18n from "./utils/i18n";
+
+// Global mock for Material UI icons using a Proxy to return a simple component
+jest.mock("@mui/icons-material", () => {
+  return new Proxy({}, {
+    get: (_, prop) => () => <div>{prop}</div>
+  });
+});
 
 test('renders welcome message', () => {
   render(
